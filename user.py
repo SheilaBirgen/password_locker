73777439c1ca4be2398262a0ebddee4c1b7fc217
SheilaBirgen <jeronobergen@gmail.com>
2020-01-26 20:55:14 +0300

    Add test _credential to check if it does exist in the contact.list

--- user.py
+++ user.py
@@ -50,3 +50,19 @@ class Credential:
         Function for saving credential
         """
         Credential.credential_list.remove(self)
+
+    
+    @classmethod
+    def credential_exists(cls, online_account):
+        '''
+        Method that checks if a credential exists from the credential list.
+        Args:
+            account_name: account_name to search if it exists
+        Returns :
+            Boolean: True or false depending if the credential exists
+        '''
+        for account in cls.credential_list:
+            if account.account_username == online_account:
+                return True
+
+        return False
--- user_test.py
+++ user_test.py
@@ -117,6 +117,23 @@ class TestCredentials(unittest.TestCase):
         test_credential.save_credential()
         self.new_credential.delete_credential()  # Deleting a user object
         self.assertEqual(len(Credential.credential_list), 1)
+        
+        
+        #Test to see if a crential exist
+    def test_credentials_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the credentials.
+        '''
+
+        self.new_credential.save_credential()
+        test_credential = Credential(
+            "test", "twitter", "test", "test")  # new contact
+        test_credential.save_credential()
+
+        credential_exists = Credential.credential_exists("test")
+
+        self.assertTrue(credential_exists)
+
 
 
 
